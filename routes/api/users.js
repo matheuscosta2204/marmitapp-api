de87e8d508584404a13a38098c8ed9bbd46689cb
Matheus Costa <matheuscosta2204@gmail.com>
2019-05-01 23:30:43 -0300

    Login Included

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
@@ -57,9 +59,21 @@ router.post(
 
             await user.save();
 
-            // Return jsonwebtoken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
 
-            return res.send('User registered');
+            jwt.sign(
+                payload, 
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
 
         } catch (err) {
             console.error(err.message);
