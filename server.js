249d140b2e5cbe68a2edbca6429f02140c0670e8
Matheus Costa <matheuscosta2204@gmail.com>
2019-11-09 11:21:36 -0300

    Added Cors Config

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
     next();
 });
 
